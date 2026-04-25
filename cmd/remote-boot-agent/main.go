9939126238a1146d81e0964d9cb02ebb5ecbb231
jeremy <jeremy@rocketscientry.com>
2026-04-25 03:00:48 +0000

    refactor: moving cli commands to their own directory

--- cmd/remote-boot-agent/main.go
+++ cmd/remote-boot-agent/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/jjack/remote-boot-agent/internal/cli"
 )
 
 func main() {
-	app := NewCLI()
+	app := cli.NewCLI()
 	if err := app.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
--- internal/cli/generate_config_form.go
+++ internal/cli/generate_config_form.go
@@ -1,4 +1,4 @@
-package main
+package cli
 
 import (
 	"charm.land/huh/v2"
